091a562a06bf6e7486af7978d50a86a0841d307b
Alexander Ekdahl <alexander@ekdahl.io>
2017-03-09 17:35:08 +0100

    Update snowtornado

---
+++ sql/README.md
@@ -0,0 +1,4 @@
+1. Import ```whalewolf``` database
+2. Create auxiliary views
+3. Execute ```psql whalewolf < export_subcategory.sql```
+4. Execute ```ruby pivot_products.rb```
---
+++ sql/export_subcategory.sql
@@ -0,0 +1,40 @@
+CREATE VIEW product_subcategory AS
+SELECT products.id,
+       products.name,
+       products.producer,
+       array_to_json(array(SELECT v.value
+                           FROM attributes
+                           LEFT JOIN (SELECT *
+                                      FROM attribute_values
+                                      WHERE attribute_values.product_id = products.id) AS v 
+                           ON attributes.id = v.attribute_id
+                           ORDER BY attributes.id))
+FROM products
+JOIN subcategories
+ON products.subcategory_id = subcategories.id
+WHERE subcategories.name = 'Mobiltelefon'
+ORDER BY products.id;
+
+CREATE VIEW matches_subcategory AS
+SELECT original_id, replacement_id, date_trunc('second', timestamp)
+FROM matches
+WHERE matches.original_id IN (SELECT id FROM product_subcategory) AND matches.replacement_id IN (SELECT id FROM product_subcategory);
+
+CREATE VIEW stock_exchange AS
+SELECT store_product_stock_exchange.aggregate_id,
+       store_product_mapping.mapped_to_upptec_product as product_id,
+       date_trunc('second', timestamp),
+       stock_status
+FROM store_product_stock_exchange
+JOIN store_product_mapping ON store_product_mapping.aggregate_id = store_product_stock_exchange.aggregate_id
+WHERE store_product_mapping.mapped_to_upptec_product IS NOT null AND store_product_mapping.mapped_to_upptec_product IN (SELECT id FROM product_subcategory)
+ORDER BY aggregate_id, timestamp;
+
+\copy (SELECT name FROM attributes ORDER BY attributes.id) TO 'attributes.csv' DELIMITER ',' CSV
+\copy (SELECT * FROM stock_exchange) TO 'stock_exchange.csv' DELIMITER ',' CSV
+\copy (SELECT * FROM matches_subcategory) TO 'matches.csv' DELIMITER ',' CSV
+\copy (SELECT * FROM product_subcategory) TO 'products.csv' DELIMITER ',' CSV
+
+DROP VIEW stock_exchange;
+DROP VIEW matches_subcategory;
+DROP VIEW product_subcategory;
