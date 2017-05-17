83a213035b7f2ea081687f7ff17daa48c1be4532
Alexander Ekdahl <alexander@ekdahl.io>
2017-05-17 10:48:02 +0200

    Potato

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 
 ## Dependencies
 
-Pip dependecies: 
+Pip dependecies:
 
 * numpy
-* pandas 
-* scipy 
+* pandas
+* scipy
 * scikit-learn
 * intervaltree
+
+High pass low-pass filter
+
+en båge som transformerar värden
+
+0.0 -> 0.0
+0.1 -> 0.2
+0.2 -> 0.8
+0.3 -> 1.0
+0.4 -> 1.0
+
+25 Augusti presentation presentation
+
+Anmäla sig som aponenter från och med den 19 Maj
+Eller 11 augusti som vi kan opponera mot
--- validation.py
+++ validation.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def train_test_split(matches, ratio, stock_exchange):
     train = matches[:round(matches.shape[0] * ratio)]
     train = train.drop_duplicates(['original_id', 'replacement_id'])
@@ -14,7 +16,7 @@ def train_test_split(matches, ratio, stock_exchange):
 
     return train, test
 
-def evaluate_model(test, find_all_matches):
+def evaluate_model(model, test, find_all_matches):
     results = []
 
     for _, row in test.iterrows():
@@ -22,7 +24,7 @@ def evaluate_model(test, find_all_matches):
         replacement_id = row['replacement_id']
         timestamp = row['timestamp']
 
-        matches = find_all_matches(original_id, timestamp)
+        matches = find_all_matches(model, original_id, timestamp)
         try:
             results.append((original_id, replacement_id, matches[0], matches.index(replacement_id)))
         except ValueError:
@@ -30,3 +32,24 @@ def evaluate_model(test, find_all_matches):
             pass
 
     return results
+
+def less_than_percentage(xs, v):
+    return '  {:.{prec}f}% less than {}'.format(len([x for x in xs if x < v]) / len(xs) * 100, v, prec=1);
+
+def output_evaluation(model, matches_test, find_all_matches):
+    ev = evaluate_model(model, matches_test, find_all_matches)
+
+    positions = list(map(lambda x: x[3], ev))
+
+    print(less_than_percentage(positions, 1))
+    print(less_than_percentage(positions, 3))
+    print(less_than_percentage(positions, 5))
+    print(less_than_percentage(positions, 15))
+
+def output_charts(model, matches_test, find_all_matches):
+    ev = evaluate_model(model, matches_test, find_all_matches)
+
+    positions = list(map(lambda x: x[3], ev))
+
+    plt.hist(positions, bins=range(0, 25, 1))
+    plt.show()
